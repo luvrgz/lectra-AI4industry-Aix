c77f839fbb2aa21386d43811a678947a76e66b11
Lucas <87275856+luvrgz@users.noreply.github.com>
2024-01-25 15:03:49 +0100

    update training 3

--- Training_model.py
+++ Training_model.py
@@ -6,8 +6,9 @@
 import pandas as pd
 import Data_analysis as data
 from sklearn.neural_network import MLPRegressor
-from sklearn.datasets import make_regression
 from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import SelectFromModel
+from sklearn.linear_model import LogisticRegression
 
 
 def finito_presto(datas):
@@ -26,19 +27,44 @@ def finito_presto(datas):
 
 def train(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.2)
-    regr = MLPRegressor(random_state=1, max_iter=500)
+    # regr = MLPRegressor(random_state=1, max_iter=500)
+    regr = LogisticRegression(max_iter=5000)
     regr.fit(X_train, y_train)
     # regr.predict(X_test)
     s = regr.score(X_test, y_test)
 
 
+def select_features(x, y):
+    # reg = MLPRegressor(random_state=1, max_iter=500)
+    model = SelectFromModel(LogisticRegression(), prefit=False)
+    model.fit(x, y)
+    return model.transform(x)
+
+
 if __name__ == "__main__":
-    datas = data.DataManager()
-    datas.load("virga")
+    # datas = data.DataManager()
+    # datas.load("virga")
 
-    y = datas.df["net_cutting_time_s"].to_numpy()
-    x = finito_presto(datas)
+    name = "VECTOR"
 
-    train(x, y)
+    dftest = pd.read_csv("..\\test_" + name + ".csv", low_memory=False)
+    dftrain = pd.read_csv("..\\train_" + name + ".csv", low_memory=False)
+
+    ytest = dftest["net_cutting_time_s"].to_numpy()
+    dftest.drop(["net_cutting_time_s"], axis=1, inplace=True)
+    xtest = dftest.to_numpy()
+
+    ytrain = dftrain["net_cutting_time_s"].to_numpy()
+    dftrain.drop(["net_cutting_time_s"], axis=1, inplace=True)
+    xtrain = dftrain.to_numpy()
+
+    regr = MLPRegressor(random_state=1, max_iter=500)
+    regr.fit(xtrain, ytrain)
+    print(regr.score(xtest, ytest))
+
+    # train(x, y)
+    # a = select_features(x, y)
+    # VIRGA: 0.9601053739149665
+    # VECTOR: 0.9579861621881663
 
     print()
