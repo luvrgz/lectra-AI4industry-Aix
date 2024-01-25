fbb7e2745e494380a1d84f508607a22a1f776366
Lucas <87275856+luvrgz@users.noreply.github.com>
2024-01-25 09:25:00 +0100

    update training 2

--- Training_model.py
+++ Training_model.py
@@ -3,22 +3,14 @@
 # @Filename: Training_model.py
 # @Author: Lucas VERGEZ
 # @Time: 24/01/2024 14:39
-from sklearn.preprocessing import LabelBinarizer
+import pandas as pd
 import Data_analysis as data
+from sklearn.neural_network import MLPRegressor
+from sklearn.datasets import make_regression
+from sklearn.model_selection import train_test_split
 
 
-def final_processing(x):
-    one_hot_enc = LabelBinarizer()
-    one_hot_enc.fit(x)
-    return one_hot_enc.transform(x)
-
-
-if __name__ == "__main__":
-    datas = data.DataManager()
-    datas.load("virga")
-
-    Y = datas.df["net_cutting_time_s"].to_numpy()
-
+def finito_presto(datas):
     datas.remove_column("net_cutting_time_s")
     datas.remove_column("Unnamed: 0")
     datas.remove_column("marker_name")
@@ -28,5 +20,25 @@ if __name__ == "__main__":
     datas.remove_column("execution")
     datas.remove_column("requirement")
     datas.remove_qualitatives(max_values=2)
-    X = final_processing(datas.df.to_numpy())
+    X = pd.get_dummies(datas.df)
+    return X.astype(dtype="float64")
+
+
+def train(X, y):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.2)
+    regr = MLPRegressor(random_state=1, max_iter=500)
+    regr.fit(X_train, y_train)
+    # regr.predict(X_test)
+    s = regr.score(X_test, y_test)
+
+
+if __name__ == "__main__":
+    datas = data.DataManager()
+    datas.load("virga")
+
+    y = datas.df["net_cutting_time_s"].to_numpy()
+    x = finito_presto(datas)
+
+    train(x, y)
+
     print()
